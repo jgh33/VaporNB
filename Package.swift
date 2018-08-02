c9fda5778abd97d6b49117a486d2439d160dbeae
焦国辉 <1035390779@qq.com>
2018-08-02 21:29:28 +0800

    更新package版本
    更新package版本

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ let package = Package(
     name: "VaporNB",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.6"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.8"),
 
         // 🖋🐘 Swift ORM (queries, models, relations, etc) built on PostgreSQL.
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.4.1"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
         
         
         // 👤 Authentication and Authorization layer for Fluent.
